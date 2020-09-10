84069c64964a030dc3a036e23bf9c357b0e2804c
Satheesh C <satheesancchandran@gmail.com>
2020-09-10 12:14:00 +0530

    Modified client.js

--- source/client.js
+++ source/client.js
@@ -1,7 +1,1 @@
-const redis = require('redis');
-const { env } = process;
-const { REDIS_URL } = env;
-const options = { db: 1, url: REDIS_URL || null };
-const client = redis.createClient(options);
-
-exports.client = client;
+exports.client = require('redis').createClient(process.env.REDIS_URL);
