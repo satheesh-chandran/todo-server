ce353e384368db06eef65186e95454469410f94e
Satheesh C <satheesancchandran@gmail.com>
2020-09-12 09:48:11 +0530

    Connected express static to build

---
+++ build/precache-manifest.f77530b0ab0ec8a28e38019308ffaf77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9487628c6bb8d6ad6f648f03ea2bcf6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a29c9cdd84fd017e392",
+    "url": "/static/css/main.3ea4cfd4.chunk.css"
+  },
+  {
+    "revision": "2dcb686a07c1b55bd83e",
+    "url": "/static/js/2.0788df6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0788df6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a29c9cdd84fd017e392",
+    "url": "/static/js/main.7e2ff0b9.chunk.js"
+  },
+  {
+    "revision": "46976c2c4ede7b9bcf75",
+    "url": "/static/js/runtime-main.a5584f08.js"
+  }
+]);
